63ffbd6f32d4b95928982411bb89163d027fe92a
Ruslan ISAKOV <46198640+RuslanIsakov@users.noreply.github.com>
2019-02-13 16:44:29 +0600

    Update Names.cs

--- Names.cs
+++ Names.cs
@@ -1,1 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("What is your name?");
+            string name;
+            name = Console.ReadLine();
+  
+            Console.WriteLine("What is your surname?");
+            string surname;
+            surname = Console.ReadLine();
+     
+            Console.WriteLine("How old are you?");
+            string age;
+            age = Console.ReadLine();
+   
+            Console.WriteLine("Where do you live?");
+            string live;
+            live = Console.ReadLine();
+
+            Console.WriteLine("Your name is "+name);
+            Console.WriteLine("Your surname is " + surname);
+            Console.WriteLine("Your age  " + age);
+            Console.WriteLine("You live in " + live);
+            
+            Console.ReadKey();
+           
+        }
+    }
+}
